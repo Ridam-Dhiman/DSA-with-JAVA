8a677fadde59503c854ef38ca130b32e24ba7b42
Ridam-Dhiman <rhythmdhiman26@gmail.com>
2025-08-02 14:19:24 +0530

    Increment

--- Learn The Basics/Learn Basic Recursion/fibonacci_2.java
+++ Learn The Basics/Learn Basic Recursion/fibonacci_2.java
@@ -1,6 +1,6 @@
 class fibonacci_2{
     public static void main(String args[]){
-        for(int i = 0; i<5; i++){
+        for(int i = 0; i<8; i++){
             System.out.print(fib(i) + " ");
         }
     }
